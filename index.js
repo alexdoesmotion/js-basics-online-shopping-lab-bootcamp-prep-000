856b9ae0781cd4b9c632748de839740e12e2e32a
Alex Johnson <alex@armadillomotion.com>
2017-11-11 17:38:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,40 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemPrice = Math.floor(Math.random()*100)
+ cart.push(new Object({[item]: itemPrice}))
+console.log(`${item} has been added to your cart.`)
+return cart
 }
 
 function viewCart() {
   // write your code here
-}
+  if (cart.length >= 1) {
+    var list = []
+    var pretext = "In your cart, you have"
+    if (cart.length === 1) {
+console.log(`${pretext} ${Object.keys(cart[0])[0]} at $${Object.values(cart[0])[0]}.`);
+        return (`${pretext} ${Object.keys(cart[0])[0]} at $${Object.values(cart[0])[0]}.`)
+    } if (cart.length === 2) {
+      console.log(`${pretext} ${Object.keys(cart[0])[0]} at $${Object.values(cart[0])[0]} and ${Object.keys(cart[1])[0]} at $${Object.values(cart[1])[0]}.`)
+      return (`${pretext} ${Object.keys(cart[0])[0]} at $${Object.values(cart[0])[0]} and ${Object.keys(cart[1])[0]} at $${Object.values(cart[1])[0]}.`)
+      } else {
+          for (let i = 0; i < cart.length; i++) {
+            if (i < cart.length - 1) {
+         list.push(` ${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]},`)
+          } else {
+          list.push(` and ${Object.keys(cart[i])[0]} at $${Object.values(cart[i])[0]}.`)
+          }
+          console.log(`${pretext}${list}`)
+          return (`${pretext}${list}`)
+        }
+      }
 
+    } else {
+    console.log(`Your shopping cart is empty.`)
+    return `Your shopping cart is empty.`
+  }
+}
 function total() {
   // write your code here
 }
