eaac8d6af607ead7bd5ab35996975eac8a61f62a
Alex Johnson <alex@armadillomotion.com>
2017-11-11 20:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber > 0) {
+    console.log(`Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`)
+    cart = 0
+  }
+  else {
+    console.log(`Sorry, we don't have a credit card on file for you.`)
+  }
 }
